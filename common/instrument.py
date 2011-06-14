a942778e65bd3cb0d24b46f2290d04dbbc5bcc55
Alex Rasmussen <alexras@acm.org>
2011-06-13 23:40:24 -0700

    Adding InstrumentProperties, which will let me create dynamic properties in Instrument's subclasses.

--- common/instrument.py
+++ common/instrument.py
@@ -2,7 +2,7 @@ import json
 import utils
 
 # Binary data for default instrument
-DEFAULT = [0xa8, 0, 0, 0xff, 0, 0, 3, 0, 0, 0xd0, 0, 0, 0, 0xf3, 0, 0]
+DEFAULT = [0, 0xa8, 0, 0, 0xff, 0, 0, 3, 0, 0, 0xd0, 0, 0, 0xf3, 0, 0]
 
 # Max. number of parameters per instrument
 NUM_PARAMS = 16
@@ -10,11 +10,27 @@ NUM_PARAMS = 16
 # Max. instrument name length
 NAME_LENGTH = 5
 
+class InstrumentProperty(object):
+    def __init__(self, byte, start_bit = 0, end_bit = 7):
+        self.byte = byte
+        self.start_bit = start_bit
+        self.end_bit = end_bit
+
+    def prop_getter(self, obj):
+        return utils.extract_bits(obj.params[self.byte], self.start_bit,
+                                  self.end_bit)
+
+    def prop_setter(self, obj, value):
+        obj.params[self.byte] = utils.inject_bits(
+            obj.params[self.byte],
+            value, self.start_bit, self.end_bit)
+
 class Instrument(object):
     def __init__(self):
         self._allocated = False
         self._name = None
         self.params = None
+        self.table = None
 
     @property
     def name(self):
@@ -42,6 +58,23 @@ class Instrument(object):
     @allocated.setter
     def allocated(self, value):
         self._allocated = bool(value)
+
+    @property
+    def instrument_type(self):
+        instr_type_byte = self.params[0]
+
+        if instr_type_byte == 0:
+            # Pulse instrument
+            pass
+        elif instr_type_byte == 1:
+            # Wave instrument
+            pass
+        elif instr_type_byte == 2:
+            # Kit instrument
+            pass
+        elif instr_type_byte == 3:
+            # Noise instrument
+            pass
 
     def __eq__(self, other):
         return type(self) == type(other) and self.params == other.params
