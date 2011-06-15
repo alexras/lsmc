e781a216b6173b1042e3a4d950412d7d4caeee97
Alex Rasmussen <alexras@acm.org>
2011-06-15 00:06:06 -0700

    Support dict-ifying tables.

--- common/table.py
+++ common/table.py
@@ -6,3 +6,11 @@ class Table(object):
         self.fx_vals = []
         self.fx2 = []
         self.fx2_vals = []
+
+    def as_dict(self):
+        write_dict = {}
+
+        for key, value in self.__dict__.items():
+            write_dict[key] = getattr(self, key)
+
+        return write_dict
