c8b8e27e486454cb94f7e2fe75ba2620056ea48b
Alex Rasmussen <alexras@trifacta.com>
2014-01-04 22:59:24 -0800

    Adding progress dialog functionality to loading

--- app/event_handlers.py
+++ app/event_handlers.py
@@ -1,6 +1,8 @@
 import wx, os
 
-def open_sav(event, songs_window):
+import common.savfile as cs
+
+def open_sav(event, main_window):
     # Display an open dialog box so the user can select a .sav file
     dlg = wx.FileDialog(None, "Choose a .sav file", '.', '',
                         '*.sav', wx.OPEN)
@@ -8,6 +10,24 @@ def open_sav(event, songs_window):
     if dlg.ShowModal() == wx.ID_OK:
         sav_file_path = os.path.join(dlg.GetDirectory(), dlg.GetFilename())
 
+        # When loading the .sav file, we'll update a progress dialog box
+        progress_dlg = wx.ProgressDialog(
+            "Loading %s" % (sav_file_path), "Starting import ...", 100)
 
+        sav_obj = None
+
+        def progress_update_function(message, step, total_steps, still_working):
+            print "PROGRESS! ", message, step, total_steps, still_working
+            progress_dlg.Update((step * 100) / total_steps, newmsg = message)
+
+        try:
+            sav_obj = cs.SAVFile(sav_file_path,
+                                 callback=progress_update_function)
+        except ValueError, e:
+            print "ERROR!", e
+
+        if sav_obj is not None:
+            main_window.update_models(sav_obj)
+            progress_dlg.Destroy()
 
     dlg.Destroy()
