9deaf9751e7f6fd1064a647e7599fc3767fda309
Alex Rasmussen <alexras@trifacta.com>
2014-05-25 18:14:59 -0700

    ImageSetViewField now accepts both dicts and lists.

--- app/ImageSetViewField.py
+++ app/ImageSetViewField.py
@@ -3,17 +3,23 @@ import wx
 from ViewField import ViewField
 
 class ImageSetViewField(ViewField):
-    def __init__(self, parent, attr_fn, image_array):
+    def __init__(self, parent, attr_fn, images):
+        if isinstance(images, list):
+            first_image = images[0]
+        elif isinstance(images, dict):
+            first_image = images[images.keys()[0]]
+
         empty_img = wx.BitmapFromImage(wx.EmptyImage(
-            image_array[0].GetWidth(), image_array[0].GetHeight()))
+            first_image.GetWidth(), first_image.GetHeight()))
+
         ViewField.__init__(
             self, parent, wx.StaticBitmap(parent, wx.ID_ANY, empty_img))
-        self.image_array = image_array
+        self.images = images
         self.attr_fn = attr_fn
 
     def update(self, data):
         instr = data
 
         self.field.SetBitmap(wx.BitmapFromImage(
-            self.image_array[self.attr_fn(instr)]))
+            self.images[self.attr_fn(instr)]))
         super(ImageSetViewField, self).update(data)
