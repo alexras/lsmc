c574402ecb572fd313fc8bf9c949f31c392d1556
Alex Rasmussen <alexras@trifacta.com>
2015-01-01 23:06:19 -0800

    Update view fields for pylsdj 1.4.0.

--- lsmc/VibeTypeViewField.py
+++ lsmc/VibeTypeViewField.py
@@ -4,12 +4,12 @@ from ImageSetViewField import ImageSetViewField
 
 from utils import make_image
 
-VIBE_IMAGES = [
-    make_image(("images", "vibe_hfsine.gif")),
-    make_image(("images", "vibe_saw.gif")),
-    make_image(("images", "vibe_sine.gif")),
-    make_image(("images", "vibe_square.gif"))
-]
+VIBE_IMAGES = {
+    "hf": make_image(("images", "vibe_hfsine.gif")),
+    "sawtooth": make_image(("images", "vibe_saw.gif")),
+    "sine": make_image(("images", "vibe_sine.gif")),
+    "square": make_image(("images", "vibe_square.gif"))
+}
 
 class VibeTypeViewField(ImageSetViewField):
     def __init__(self, parent):
--- lsmc/WaveViewField.py
+++ lsmc/WaveViewField.py
@@ -6,11 +6,12 @@ from viewutils import instr_attr
 
 from utils import make_image
 
-WAVE_IMAGES = [
-    make_image(("images", "wave12.5.gif")),
-    make_image(("images", "wave25.gif")),
-    make_image(("images", "wave50.gif")),
-    make_image(("images", "wave75.gif"))]
+WAVE_IMAGES = {
+    "12.5%": make_image(("images", "wave12.5.gif")),
+    "25%": make_image(("images", "wave25.gif")),
+    "50%": make_image(("images", "wave50.gif")),
+    "75%": make_image(("images", "wave75.gif"))
+}
 
 class WaveViewField(ImageSetViewField):
     def __init__(self, parent):
