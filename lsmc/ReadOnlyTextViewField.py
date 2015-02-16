83d644185bb65dbebe24e923592ef8a2d0152c17
Alex Rasmussen <alexras@acm.org>
2015-02-16 10:43:16 -0800

    Make read-only text controls static.
    This should avoid confusion as to what is editable and what isn't.

--- lsmc/ReadOnlyTextViewField.py
+++ lsmc/ReadOnlyTextViewField.py
@@ -7,11 +7,11 @@ class ReadOnlyTextViewField(ViewField):
 
     def __init__(self, parent, format_fn):
         ViewField.__init__(
-            self, parent, wx.TextCtrl(parent, style=wx.TE_READONLY))
+            self, parent, wx.StaticText(parent))
         self.format_fn = format_fn
 
     def update(self, data):
         instr = data
 
-        self.field.SetValue(self.format_fn(instr))
+        self.field.SetLabel(self.format_fn(instr))
         super(ReadOnlyTextViewField, self).update(data)
